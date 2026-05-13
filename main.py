51a3cfa8cae81a90ad014b4a36a758bd726602da
Haval121 <havalhavo54@gmail.com>
2026-05-14 00:04:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ URL_REGEX = re.compile(r'https?://\S+|t\.me/\S+|www\.\S+', re.IGNORECASE)
 
 logging.basicConfig(level=logging.INFO)
 
+videos = []
+
 
 async def delete_msg(bot, chat_id, msg_id):
     try:
@@ -21,18 +23,38 @@ async def delete_msg(bot, chat_id, msg_id):
         pass
 
 
-async def resend_loop(bot, chat_id, file_id, caption):
+async def cycle(bot, chat_id):
     while True:
-        await asyncio.sleep(RESEND_DELAY)
-
-        sent = await bot.send_video(
-            chat_id=chat_id,
-            video=file_id,
-            caption=caption
-        )
 
         await asyncio.sleep(DELETE_DELAY)
-        await delete_msg(bot, chat_id, sent.message_id)
+
+        for v in videos:
+            await delete_msg(bot, chat_id, v["msg_id"])
+
+        await asyncio.sleep(RESEND_DELAY)
+
+        new_videos = []
+
+        tasks = [
+            bot.send_video(
+                chat_id,
+                video=v["file_id"],
+                caption=v["caption"]
+            )
+            for v in videos
+        ]
+
+        sent_msgs = await asyncio.gather(*tasks)
+
+        for i, sent in enumerate(sent_msgs):
+            new_videos.append({
+                "msg_id": sent.message_id,
+                "file_id": videos[i]["file_id"],
+                "caption": videos[i]["caption"]
+            })
+
+        videos.clear()
+        videos.extend(new_videos)
 
 
 async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -47,20 +69,14 @@ async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
 
     if msg.video:
-        file_id = msg.video.file_id
-        caption = msg.caption
+        videos.append({
+            "msg_id": msg.message_id,
+            "file_id": msg.video.file_id,
+            "caption": msg.caption
+        })
 
-        await asyncio.sleep(DELETE_DELAY)
-        await delete_msg(context.bot, msg.chat_id, msg.message_id)
-
-        asyncio.create_task(
-            resend_loop(
-                context.bot,
-                msg.chat_id,
-                file_id,
-                caption
-            )
-        )
+        if len(videos) == 1:
+            asyncio.create_task(cycle(context.bot, msg.chat_id))
 
 
 def main():
